bc0ceedf4a4111a577e8fc18d5197b3e36cf92fd
Jacek <jacekszwarc1@gmail.com>
2024-02-01 20:26:37 +0100

    User Model and Registration (update views, templates, forms)

--- TaskManager/accounts/views.py
+++ TaskManager/accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Redirect to the desired page after registration
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'register.html', {'form': form})
