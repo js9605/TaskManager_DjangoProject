fba7edae8d518e3ac4bcea76f522fe2042ab5672
Jacek <jacekszwarc1@gmail.com>
2024-02-01 18:41:22 +0100

    Create custom User model

--- TaskManager/TaskManager/settings.py
+++ TaskManager/TaskManager/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'task_manager_app',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# for accounts (custom User model)
+AUTH_USER_MODEL = 'accounts.CustomUser'
