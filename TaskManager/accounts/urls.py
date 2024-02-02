216ec583d99354b53d9495b44897e2bb01fc1d4e
Jacek <jacekszwarc1@gmail.com>
2024-02-02 18:51:37 +0100

    UserRegistrationTest

--- TaskManager/accounts/urls.py
+++ TaskManager/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import register
+from .views import register, custom_login
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/', custom_login, name='login'),
 ]
--- TaskManager/accounts/views.py
+++ TaskManager/accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+
 from .forms import RegistrationForm
 
 def register(request):
@@ -8,8 +11,28 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')  # Redirect to the desired page after registration
+            print("DEBUG here")
+            return redirect('login')
     else:
         form = RegistrationForm()
 
     return render(request, 'register.html', {'form': form})
+
+#TODO I get Please enter a correct username and password. Note that both fields may be case-sensitive. after correct username and password
+def custom_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome back, {username}!')
+                return redirect('home') 
+            else:
+                messages.error(request, 'Invalid username or password.')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
