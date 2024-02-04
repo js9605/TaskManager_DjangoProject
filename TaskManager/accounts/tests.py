a9787855b86bf4216a7e7c88be2cffd627ad54d6
Jacek <jacekszwarc1@gmail.com>
2024-02-04 14:37:44 +0100

    Add logs and custom_logout

--- TaskManager/accounts/tests.py
+++ TaskManager/accounts/tests.py
@@ -2,23 +2,22 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
+
 class UserRegistrationTest(TestCase):
     def test_user_registration_view(self):
-        url = reverse('register')
+        url = reverse("register")
         response = self.client.get(url)
 
-        self.assertTemplateUsed(response, 'register.html')
+        self.assertTemplateUsed(response, "register.html")
         self.assertEqual(response.status_code, 200)
 
         user_data = {
-            'username': 'testuser2',
-            'password1': 'testpassword2',
-            'password2': 'testpassword2',
+            "username": "testuser",
+            "password1": "testpassword",
+            "password2": "testpassword",
         }
 
         response = self.client.post(url, data=user_data, follow=True)
 
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'register.html')
-
-
+        self.assertTemplateUsed(response, "register.html")
--- TaskManager/accounts/urls.py
+++ TaskManager/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register, custom_login
+from .views import register, custom_login, custom_logout
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', custom_login, name='login'),
+    path('logout/', custom_logout, name='logout'),
 ]
